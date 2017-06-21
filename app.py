4758e53b9e33618be4ace12e0f9d18e600cb92c7
adrian <adrian@eventbase.com>
2017-06-21 08:51:32 -0700

    fix app2

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-import db
+from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
